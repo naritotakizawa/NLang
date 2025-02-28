a22112be106cccb1c4eb5386e0c9a9bd7340e4f7
Narito Takizawa <toritoritorina@gmail.com>
2025-02-28 16:29:26 +0900

    Update and rename main.py to nlang.py

--- src/nlang.py
+++ src/nlang.py
@@ -11,6 +11,33 @@ from ast_nodes import ast_to_dict
 from codegen import BytecodeGenerator
 from vm import VirtualMachine
 
+def run(code, debug=True):
+    # トークナイズ
+    tokens = tokenize(code)
+    if debug:
+        print_tokens(tokens)  # トークンを出力
+
+    # パース
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+    # AST を出力
+    if debug: 
+        print("AST:")
+        print(json.dumps(ast_to_dict(ast), indent=2, ensure_ascii=False))
+
+    # バイトコード生成
+    generator = BytecodeGenerator()
+    bytecode = generator.generate(ast)
+    if debug: 
+        print("Bytecode:")
+        for instr in bytecode:
+            print(f"  {instr}")
+
+    # 仮想マシンで実行
+    vm = VirtualMachine()
+    vm.execute(bytecode)    
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: python main.py <source_file>")
@@ -20,29 +47,8 @@ def main():
     source_file = sys.argv[1]
     with open(source_file, "r", encoding="utf-8") as f:
         code = f.read()
+    run(code)
 
-    # トークナイズ
-    tokens = tokenize(code)
-    print_tokens(tokens)  # トークンを出力
-
-    # パース
-    parser = Parser(tokens)
-    ast = parser.parse()
-
-    # AST を出力
-    print("AST:")
-    print(json.dumps(ast_to_dict(ast), indent=2, ensure_ascii=False))
-
-    # バイトコード生成
-    generator = BytecodeGenerator()
-    bytecode = generator.generate(ast)
-    print("Bytecode:")
-    for instr in bytecode:
-        print(f"  {instr}")
-
-    # 仮想マシンで実行
-    vm = VirtualMachine()
-    vm.execute(bytecode)
 
 if __name__ == "__main__":
     main()
