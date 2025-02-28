96e5b63d5acdb1cc6cc62e2840d254a72466439e
Narito Takizawa <toritoritorina@gmail.com>
2025-02-28 14:01:19 +0900

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -40,7 +40,7 @@ class Parser:
         # 変数代入のチェック (IDENTIFIER "a = 1" ...)
         if self.pos + 1 < len(self.tokens):            
             next_token = self.tokens[self.pos + 1]
-            if token[0] == "IDENTIFIER" and next_token][0] == "PUNCT" and next_token[1] == "=":
+            if token[0] == "IDENTIFIER" and next_token[0] == "PUNCT" and next_token[1] == "=":
                 return self.parse_assignment()
 
     
